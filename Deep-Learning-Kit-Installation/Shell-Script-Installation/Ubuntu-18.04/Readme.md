c82e4a503fcd140c3678c879945b4c277846c2d9
amir-jafari <amir.h.jafari@okstate.edu>
2019-05-26 21:07:30 -0400

    Updated

---
+++ Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-18.04/Readme.md
@@ -0,0 +1,99 @@
+# Ubuntu 18.04 Deep Learning Software shell script
+
+## Getting Started 
+
+Version 3: To install all the Frameworks (tensorflow, theano, pytorch, Keras), launch your VM  and run the following commands in order in to your VM terminal (CUDA 8).
+
+
+
+```
+sudo apt-get install git -y
+```
+```
+git clone https://github.com/amir-jafari/Cloud-Computing.git
+```
+```
+cd Cloud-Computing/Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-18.04/
+```
+We are going to install Version 3:
+
+```
+mv install-18-04-final-V3.sh ~
+```
+```
+cd ~
+```
+```
+chmod +x install-18-04-final-V3.sh
+```
+```
+sudo ./install-18-04-final-V3.sh
+```
+
+## Testing the framworks
+
+* Set Environment
+
+Run the following commands
+
+```
+source /etc/environment
+```
+```
+source ~/.bashrc
+```
+
+* Tensorflow, Keras, Theano (Virtual Python 2.7)
+
+Enter the following command on your terminal
+
+```
+python3
+```
+then in the python env write
+```
+import tensorflow
+```
+```
+import keras
+```
+```
+import theano
+```
+if you did not get any error then exit out from python by exit().
+
+
+
+* Pytorch 
+
+Enter the following command on your terminal
+```
+python
+```
+then in the python env write
+```
+import torch
+```
+```
+import torchvision
+```
+
+if you did not get any error then exit out from python env by exit(). 
+
+
+* Pycharm 
+
+Note: Mac users need to acivate [Xquartz](https://www.xquartz.org/) in their machine and then open your terminal. In other words, when you are ssh ing to VM use -X as follows:
+
+```
+ssh -X -i <private key file > <netid>@<External Ip address>
+``` 
+
+Note: Windows users use Mobaexterma and you are fine.
+
+To activate pycharm enter the following commands 
+
+```
+./pycharm.sh
+```
+
---
+++ Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-18.04/install-18-04-final-V3.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# --------------------------------------------------------------#
+# Script to set up a Deep Learning VM on Google Cloud Platform	#
+#---------------------------------------------------------------#
+#Autor: Amir Jafari		                                    	#
+#Date: 05/26/2019						                        #
+#                                                               #
+# INSTRUCTIONS: When you run this script, make sure you         #
+# include the username associated with your instance as         #
+# the first parameter. Otherwise, the softwares will not        #
+# work properly.   							                    #
+# ------------------------------------------------------------- #
+# ----------------- Browser ------------------
+sudo apt update
+sudo apt upgrade -y
+# ----------------- Chromium-------------------
+sudo apt install chromium-browser -y
+# ----------------- Cuda 10.0 -----------------
+wget https://storage.googleapis.com/cuda-deb/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda -y
+sed 1d /etc/environment > /etc/environment
+echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/cuda-10.0/bin"' >> /etc/environment
+source /etc/environment
+nvcc --version
+nvidia-smi
+
+# ----------------- Cudnn 7.6 -----------------
+wget https://storage.googleapis.com/cuda-deb/libcudnn7-dev_7.5.0.56-1%2Bcuda10.0_amd64.deb
+wget https://storage.googleapis.com/cuda-deb/libcudnn7-doc_7.5.0.56-1%2Bcuda10.0_amd64.deb
+wget https://storage.googleapis.com/cuda-deb/libcudnn7_7.5.0.56-1%2Bcuda10.0_amd64.deb
+
+sudo sudo dpkg -i libcudnn7-dev_7.5.0.56-1+cuda10.0_amd64.deb
+sudo dpkg -i libcudnn7-doc_7.5.0.56-1+cuda10.0_amd64.deb
+sudo dpkg -i libcudnn7_7.5.0.56-1+cuda10.0_amd64.deb
+
+cp -r /usr/src/cudnn_samples_v7/ $HOME
+cd $HOME/cudnn_samples_v7/mnistCUDNN
+make clean && make
+./mnistCUDNN
+cd ~
+# ----------------- Python 3.6 ------------------------------------
+sudo apt install -y python3-pip
+sudo apt install build-essential libssl-dev libffi-dev python3-dev
+sudo pip3 install --upgrade pip
+sudo -H pip3 install matplotlib
+sudo -H pip3 install pandas
+sudo -H pip3 install h5py
+sudo -H pip3 install leveldb
+sudo -H pip3 install seaborn
+sudo -H pip3 install --upgrade tensorflow-gpu
+sudo -H pip3 install Theano 
+sudo -H pip3 install keras
+sudo -H pip3 install sklearn
+sudo -H pip3 install cython
+sudo -H pip3 install torch 
+sudo -H pip3 install torchvision
+sudo -H pip3 install opencv-python
+sudo -H pip3 install lmdb
+sudo -H pip3 install sympy
+sudo -H pip3 install pydotplus
+sudo apt-get install -y p7zip-full
+sudo apt install unzip
+
+# ----------------- Pycharm 2019 -----------------
+wget https://storage.googleapis.com/cuda-deb/pycharm-community-2019.1.2.tar.gz
+sudo tar -zxf pycharm-community-2019.1.2.tar.gz
+sudo ln -s /home/ubuntu/pycharm-community-2019.1.2/bin/pycharm.sh pycharm
