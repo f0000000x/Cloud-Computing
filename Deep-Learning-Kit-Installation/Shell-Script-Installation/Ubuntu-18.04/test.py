6de8346bc0d47ba5bba5202feb4189ad1e0b2e49
amir-jafari <amir.h.jafari@okstate.edu>
2019-05-29 08:39:59 -0400

    Updated

--- Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-18.04/test.py
+++ Deep-Learning-Kit-Installation/Shell-Script-Installation/Ubuntu-18.04/test.py
@@ -1,28 +1,30 @@
 print(20*'-'+'Checking Deep Learning Frameworks'+20*'-')
 import os
 print(20*'-'+'Nvidia Cuda'+20*'-')
-
+# %%------------------------------------------------------------
 os.system("nvidia-smi")
 print(20*'.'+20*'.'+20*'.')
 os.system("nvcc --version")
 print(20*'.'+20*'.'+20*'.')
 os.chdir('/home/ubuntu/cudnn_samples_v7/mnistCUDNN')
 os.system("./mnistCUDNN")
-
+# %%------------------------------------------------------------
 print(20*'-'+'Python Packages'+20*'-')
-
+# %%------------------------------------------------------------
 try:
     import matplotlib
     print('matplotlib' + '---->' + 'Test Passed')
 except:
     print('matplotlib IS NOT INSTALLED')
 print(20*'.'+20*'.'+20*'.')
+# %%------------------------------------------------------------
 try:
     import numpy
     print('numpy' + '---->' + 'Test Passed')
 except:
     print('numpy IS NOT INSTALLED')
 print(20*'.'+20*'.'+20*'.')
+# %%------------------------------------------------------------
 try:
     import torch
     print('torch' + '---->' + 'Test Passed')
